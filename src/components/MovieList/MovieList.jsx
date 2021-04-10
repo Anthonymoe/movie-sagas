4bf6a80df42e47f63e71d4e1ec68d355486d0306
Anthony Moe <anthony.moe7@gmail.com>
2021-04-10 12:51:53 -0500

    movie poster click handler brings to detail page, need to make it unique to selected movie

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './MovieList.css'
 
 function MovieList() {
 
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
+    let history =  useHistory();
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
+
+    const showDetails = () => {
+        history.push('/details')
+    }
 
     return (
         <main>
@@ -17,7 +23,7 @@ function MovieList() {
             <section className="movies">
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} >
+                        <div onClick={showDetails} key={movie.id} >
                             <h3>{movie.title}</h3>
                             <img src={movie.poster} alt={movie.title}/>
                             {/* testing how to make the selector for the details page */}
